fea95c03e874b5e0f6eb4a2d6888e94a32475cd2
dempss <jack.r.dempster@accenture.com>
2019-04-04 14:55:39 +0100

    Updates

---
+++ precache-manifest.0e2c48a7842c80e228cf02c504232585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e6b2e94320e3a386ea4",
+    "url": "/TestApp/static/js/runtime~main.8985dab4.js"
+  },
+  {
+    "revision": "77a71b524a872ce21010",
+    "url": "/TestApp/static/js/main.379db33e.chunk.js"
+  },
+  {
+    "revision": "a397e4f191bc78c9bd60",
+    "url": "/TestApp/static/js/2.c40744a1.chunk.js"
+  },
+  {
+    "revision": "77a71b524a872ce21010",
+    "url": "/TestApp/static/css/main.3f9051a6.chunk.css"
+  },
+  {
+    "revision": "0b61b4fe060f6f985c28c96493108112",
+    "url": "/TestApp/index.html"
+  }
+];
